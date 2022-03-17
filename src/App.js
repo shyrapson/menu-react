c80701d52a63b0738d477c91719439c667eee822
shyrapson <meetshyrapson@gmail.com>
2022-03-17 12:03:49 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React,{useState} from 'react'
+import items from './data'
+import Menu from './Menu';
+
+import Categories from './Categories';
 import './App.css';
 
+// adding button dynamically
+// new Set() method is use to get only uniq values to avoid repetetion.
+// it prevent values from repeating twice or more
+// adding 'all' to the begining of the value in an array is you create
+//  an array will 'all' and the copy the uniq set
+const allCategories =['all',...new Set(items.map((item)=> item.category)) ];
+console.log(allCategories)
+// pass allCategories to catgories stale below
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if(category ==='all') {
+      setMenuItems(items)
+     return category;
+    }
+    const newItems = items.filter((item) => item.category === category)
+    setMenuItems(newItems)
+  }
+  return <main>
+   <section className="menu" >
+     <div className="title">
+     <h2>our menu</h2>
+     <div className="underline"></div>
+     </div>
+        <Categories categories={categories} filterItems={filterItems}/>
+        <Menu items={menuItems}/>
+   </section>
+  </main>
+
 }
 
 export default App;
